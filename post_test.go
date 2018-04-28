2c3384bd5bfe6da3e1ebf9b2af339de644a510d8
Jacob Meixner <jjm3333@gmail.com>
2018-04-28 16:22:34 -0700

    Add some more tests arround create post

--- post_test.go
+++ post_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestCreatePost(t *testing.T) {
+func TestCreatePostReadsBody(t *testing.T) {
 
 	//setup
 	c := &tacitContextMock{
@@ -19,11 +19,52 @@ func TestCreatePost(t *testing.T) {
 	if !c.bindJSONIsCalled {
 		t.Error("bindJSON is never called and should be called at least once")
 	}
+
+}
+
+func TestCreatePostHapyPath(t *testing.T) {
+
+	//setup
+	c := &tacitContextMock{
+		jsonCode:         0,
+		timesJSONisCaled: 0,
+	}
+	db := &tacitDBMock{}
+
+	//execution
+	createPost(c, db)
+
+	//assertions
+	if c.jsonCode != 200 {
+		t.Error("The expected http status code is 200 for happy path")
+	}
+
+	if c.timesJSONisCaled != 1 {
+		t.Errorf("json should be called on the context exactly once but instead was called %v time(s)", c.timesJSONisCaled)
+	}
+
+}
+
+func TestCreatePostSavesPost(t *testing.T) {
+
+	//setup
+	c := &tacitContextMock{}
+	db := &tacitDBMock{timesCreateWasCalled: 0}
+	expectedDbCreates := 1
+
+	//execution
+	createPost(c, db)
+
+	//assertions
+	if db.timesCreateWasCalled != expectedDbCreates {
+		t.Errorf("db.create is expected to be called %v time(s) but instead was called %v time(s)", expectedDbCreates, db.timesCreateWasCalled)
+	}
 }
 
 type tacitContextMock struct {
 	bindJSONIsCalled bool
 	jsonCode         int
+	timesJSONisCaled int
 }
 
 func (ctx *tacitContextMock) bindJSON(obj interface{}) error {
@@ -37,4 +78,5 @@ func (ctx *tacitContextMock) readBody([]byte) (int, error) {
 
 func (ctx *tacitContextMock) json(code int, obj map[string]interface{}) {
 	ctx.jsonCode = code
+	ctx.timesJSONisCaled++
 }
