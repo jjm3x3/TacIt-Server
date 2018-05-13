41be5682169a0a4c3a1685f41227248b94b59183
Jacob Meixner <jjm3333@gmail.com>
2018-05-13 15:07:59 -0700

    Remove homebrew logger mock update all tests to use gomock logger

--- post_test.go
+++ post_test.go
@@ -6,89 +6,22 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/sirupsen/logrus"
-	// "fmt"
 )
-
-type loggerMock struct {
-	errorMessage string
-}
-
-func (logger *loggerMock) WithField(key string, value interface{}) *logrus.Entry {
-	return nil
-}
-
-func (logger *loggerMock) WithFields(fields logrus.Fields) *logrus.Entry {
-	return nil
-}
-
-func (logger *loggerMock) WithError(err error) *logrus.Entry {
-	return nil
-}
-
-func (logger *loggerMock) Debugf(format string, args ...interface{}) {
-}
-func (logger *loggerMock) Infof(format string, args ...interface{}) {
-}
-func (logger *loggerMock) Printf(format string, args ...interface{}) {
-}
-func (logger *loggerMock) Warnf(format string, args ...interface{}) {
-}
-func (logger *loggerMock) Warningf(format string, args ...interface{}) {
-}
-func (logger *loggerMock) Errorf(format string, args ...interface{}) {
-	logger.errorMessage = format
-}
-func (logger *loggerMock) Fatalf(format string, args ...interface{}) {
-}
-func (logger *loggerMock) Panicf(format string, args ...interface{}) {
-}
-
-func (logger *loggerMock) Debug(args ...interface{}) {
-}
-func (logger *loggerMock) Info(args ...interface{}) {
-}
-func (logger *loggerMock) Print(args ...interface{}) {
-}
-func (logger *loggerMock) Warn(args ...interface{}) {
-}
-func (logger *loggerMock) Warning(args ...interface{}) {
-}
-func (logger *loggerMock) Error(args ...interface{}) {
-}
-func (logger *loggerMock) Fatal(args ...interface{}) {
-}
-func (logger *loggerMock) Panic(args ...interface{}) {
-}
-
-func (logger *loggerMock) Debugln(args ...interface{}) {
-}
-func (logger *loggerMock) Infoln(args ...interface{}) {
-}
-func (logger *loggerMock) Println(args ...interface{}) {
-}
-func (logger *loggerMock) Warnln(args ...interface{}) {
-}
-func (logger *loggerMock) Warningln(args ...interface{}) {
-}
-func (logger *loggerMock) Errorln(args ...interface{}) {
-}
-func (logger *loggerMock) Fatalln(args ...interface{}) {
-}
-func (logger *loggerMock) Panicln(args ...interface{}) {
-}
 
 func TestCreatePostReadsBody(t *testing.T) {
 
 	//setup
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockLogger := mocks.NewMockFieldLogger(mockCtrl)
+
 	c := &httpContextMock{
 		bindJSONIsCalled: false,
 	}
 	db := &tacitDb.TacitDBMock{}
-	logger := &loggerMock{}
 
 	//execution
-	createPost(c, db, logger)
+	createPost(c, db, mockLogger)
 
 	//assertions
 	if !c.bindJSONIsCalled {
@@ -100,17 +33,15 @@ func TestCreatePostReadsBody(t *testing.T) {
 func TestCreatePostHapyPath(t *testing.T) {
 
 	//setup
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockLogger := mocks.NewMockFieldLogger(mockCtrl)
+
 	c := &httpContextMock{
 		jsonCode:          0,
 		timesJSONisCalled: 0,
 	}
 	db := &tacitDb.TacitDBMock{}
-
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockLogger := mocks.NewMockFieldLogger(mockCtrl)
-
-	// logger := &loggerMock{}
 
 	//execution
 	createPost(c, db, mockLogger)
@@ -129,6 +60,10 @@ func TestCreatePostHapyPath(t *testing.T) {
 func TestCreatePostSadPath(t *testing.T) {
 
 	//setup
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockLogger := mocks.NewMockFieldLogger(mockCtrl)
+
 	c := &httpContextMock{
 		jsonCode:          0,
 		timesJSONisCalled: 0,
@@ -137,13 +72,8 @@ func TestCreatePostSadPath(t *testing.T) {
 
 	db := &tacitDb.TacitDBMock{}
 
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	mockLogger := mocks.NewMockFieldLogger(mockCtrl)
-
-	mockLogger.EXPECT().Error("There was no body provided")
+	mockLogger.EXPECT().Error(gomock.Any()).AnyTimes()
 	mockLogger.EXPECT().Errorf("There was an error binding to aPost: %v", gomock.Any())
-	// logger := &loggerMock{}
 
 	//execution
 	createPost(c, db, mockLogger)
@@ -160,14 +90,16 @@ func TestCreatePostSadPath(t *testing.T) {
 func TestCreatePostSavesPost(t *testing.T) {
 
 	//setup
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockLogger := mocks.NewMockFieldLogger(mockCtrl)
+
 	c := &httpContextMock{}
 	db := &tacitDb.TacitDBMock{TimesCreateWasCalled: 0}
 	expectedDbCreates := 1
 
-	logger := &loggerMock{}
-
 	//execution
-	createPost(c, db, logger)
+	createPost(c, db, mockLogger)
 
 	//assertions
 	if db.TimesCreateWasCalled != expectedDbCreates {
@@ -178,19 +110,22 @@ func TestCreatePostSavesPost(t *testing.T) {
 func TestCreatePostBindJSONFailureLogsError(t *testing.T) {
 
 	//setup
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	mockLogger := mocks.NewMockFieldLogger(mockCtrl)
+
 	c := &httpContextMock{
 		bindJSONDoesError: true,
 	}
 	db := &tacitDb.TacitDBMock{}
 
-	logger := &loggerMock{}
+	expectedMessage := "There was an error binding to aPost: %v"
+	mockLogger.EXPECT().Error("There was no body provided")
+	mockLogger.EXPECT().Errorf(expectedMessage, gomock.Any())
 
 	//execution
-	createPost(c, db, logger)
+	createPost(c, db, mockLogger)
 
-	expectedMessage := "There was an error binding to aPost: %v"
 	//assertions
-	if logger.errorMessage != expectedMessage {
-		t.Errorf("In the case of a bindJSON error the logger is expected to log the fallowing '%v' but instead logged the message: '%v'", expectedMessage, logger.errorMessage)
-	}
+	// taken care of through gomock
 }
