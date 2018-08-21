3a2684d97ba981e0e4dc7babeac6284fe3e44917
Jacob Meixner <jjm3333@gmail.com>
2018-08-20 21:36:59 -0700

    Make a trouble shooting build

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	// "fmt"
 	"os"
 
-	"tacit-api/db"
 	"tacit-api/crypt"
+	"tacit-api/db"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -46,22 +45,24 @@ func main() {
 	// defaultHost := "localhost"
 	// defaultPort := "5432"
 	dbUser := os.Getenv("DB_USER")
+	aLogger.Info("The user is: ", dbUser)
 	if len(dbUser) == 0 {
 		dbUser = "gorm"
 	}
 	defaultDb := "tacit_db"
 	dbPassword := os.Getenv("DB_PASSWORD")
+	aLogger.Info("the password is: ", dbPassword)
 	if len(dbPassword) == 0 {
 		dbPassword = "@"
 	}
 
-	connectionString := dbUser + ":" + dbPassword + "@tcp(127.0.0.1:3306)/" + defaultDb + "?charset=utf8&parseTime=True&loc=Local"
+	connectionString := dbUser + ":" + dbPassword + "@tcp(127.0.0.1:33306)/" + defaultDb + "?charset=utf8&parseTime=True&loc=Local"
 	// connectionString := "host="+defaultHost+" port="+defaultPort+" user="+defaultUser+" dbname="+defaultDb+" sslmode=disable"
 	dbHandle, err := gorm.Open("mysql", connectionString) // TODO:: enable ssl
 	defer dbHandle.Close()
 
 	if err != nil {
-		aLogger.Errorln("There was an error opeing the db: ", err)
+		aLogger.Errorln("There was an error opening the db: ", err)
 		// TODO :: should exit right away
 	}
 
