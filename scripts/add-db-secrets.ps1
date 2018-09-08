848f1154cac2f18343083931e3347a5f3e19a837
Jacob Meixner <jjm3333@gmail.com>
2018-09-08 15:31:37 -0700

    Improve the script to be relative to where its run
    which should be in its directory. Also this can now be checked in
    without a token in it since it will read the password out of  .txt file
    called password.txt

--- scripts/add-db-secrets.ps1
+++ scripts/add-db-secrets.ps1
@@ -1,2 +1,4 @@
-kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=D:\Users\jmeixner\Code\goCode\src\TacIt-go\config\tacit-db67b0097365.json
-kubectl create secret generic cloudsql-db-credentials --from-literal=username=gorm --from-literal=password=<redacted>
+$projectRoot=Split-Path $(pwd)
+
+kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=$projectRoot\config\tacit-db67b0097365.json
+kubectl create secret generic cloudsql-db-credentials --from-literal=username=gorm --from-file=./password.txt
