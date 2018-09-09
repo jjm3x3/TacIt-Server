40436cc3efa96565685be163b1378f31275b5655
Jacob Meixner <jjm3333@gmail.com>
2018-09-09 11:30:24 -0700

    Make the setup script even friendlier

--- scripts/setupEnv.ps1
+++ scripts/setupEnv.ps1
@@ -1,1 +1,8 @@
 $Env:DB_PORT=33306
+$Env:GOPATH=Split-Path $(Split-Path $(pwd))
+$pwPath='.\kube\password.txt'
+if(!(Test-Path $pwPath)){
+	Write-Error "$pwPath file missing"
+} else {
+	$Env:DB_PASSWORD=$(Get-Content $pwPath)
+}
