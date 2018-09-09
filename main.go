c5546ab7cacb117eaf1f98ab395979bf4ef74bd8
Jacob Meixner <jjm3333@gmail.com>
2018-09-09 16:13:22 -0700

    Pull JWT out of header and attempt to parse it

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	// "fmt"
+	"fmt"
 	"os"
 
 	"tacit-api/crypt"
@@ -64,7 +65,7 @@ func main() {
 		dbPort = "3306"
 	}
 
-	connectionString := dbUser + ":" + dbPassword + "@tcp(127.0.0.1:" + dbPort +")/" + defaultDb + "?charset=utf8&parseTime=True&loc=Local"
+	connectionString := dbUser + ":" + dbPassword + "@tcp(127.0.0.1:" + dbPort + ")/" + defaultDb + "?charset=utf8&parseTime=True&loc=Local"
 	// connectionString := "host="+defaultHost+" port="+defaultPort+" user="+defaultUser+" dbname="+defaultDb+" sslmode=disable"
 	dbHandle, err := gorm.Open("mysql", connectionString) // TODO:: enable ssl
 	defer dbHandle.Close()
@@ -78,11 +79,15 @@ func main() {
 		GormDB: dbHandle,
 	}
 
-	db.RunMigration(aRealTacitDB)
+	// db.RunMigration(aRealTacitDB)
 
 	r := gin.Default()
+
+	r.Use(FUCKITIWRITEMYOWN())
+
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
+		response := c.MustGet("thing").(string)
+		c.JSON(200, gin.H{"message": "pong: " + response})
 	})
 	anEnv := &env{ourDB: aRealTacitDB, logger: aLogger, ourCrypt: &crypt.RealTacitCrypt{}}
 
@@ -95,4 +100,30 @@ func main() {
 	r.GET("/note", anEnv.doListPosts)
 
 	r.Run()
+}
+
+func FUCKITIWRITEMYOWN() gin.HandlerFunc {
+	fmt.Println("installing a middleware")
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+			return "", nil
+		})
+		if err != nil {
+			fmt.Println("There was an issue parsing the JWT")
+		}
+		// Don't forget to validate the alg is what you expect:
+		// token, err := jwt.ParseWithClaims(tokenString, &CustomClaimsExample{}, func(token *jwt.Token) (interface{}, error) {
+		// 	// since we only use the one private key to sign the tokens,
+		// 	// we also only use its public counter part to verify
+		// 	return verifyKey, nil
+		// })
+		c.Set("thing", tokenString)
+		fmt.Printf("IN A MIDDLEWARE BITCH with: '%v'\n", tokenString)
+	}
 }
